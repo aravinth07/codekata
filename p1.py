1d506125a40243ed6fb88a6aa53ff6adb502a8bf
aravinth07 <37236641+aravinth07@users.noreply.github.com>
2018-03-10 13:11:26 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      rgcet
+#
+# Created:     10/03/2018
+# Copyright:   (c) rgcet 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import math
+
+number = int (input ('Enter number: '))
+if number % 2 == 0 and number != 0:
+        print ('Even number')
+else:
+         print ('Odd number')
+
